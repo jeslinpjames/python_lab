4794049b8d487165b7dc1b4e859b582ef631775b
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-02 17:47:28 +0530

    added min and max area

--- Lab_Cycle_2/q3.py
+++ Lab_Cycle_2/q3.py
@@ -7,12 +7,14 @@ def read_json(file_path):
     for x in listdata:
         print(x,'\n')
     return listdata
+
 def list_to_dict(filepath):
     with open(filepath,'r') as f:
         dictdata=json.load(f)
     print("The file as a list of dictionary objects is : \n")
     print (dictdata)
     return dictdata
+
 def show_setosa(filepath):
     with open(filepath,'r')as f:
         data_list=json.load(f)
@@ -21,7 +23,30 @@ def show_setosa(filepath):
         if x["species"]=='setosa':
             print (x)
 
+def min_max_area(path):
+    with open(path,'r') as f:
+        datalist=json.load(f)
+    species_names=[]
+    for x in datalist:
+        species_names.append(x["species"])
+    species_names=list(set(species_names))
+    sepal_area=[]
+    petal_area=[]
+    for i in species_names:
+        for j in datalist:
+            if j['species']==i:
+                sepal_area.append(j['sepalLength']*j['sepalWidth'])
+                petal_area.append(j['petalLength']*j['petalLength'])
+        print("Maximum sepal area in ",i," species is : ",round(max(sepal_area),2))
+        print("Minimum petal area in ",i," species is : ",round(min(petal_area)),2)
+        sepal_area.clear()
+        petal_area.clear()       
+
+    print(species_names)    
+
+
 listdata=read_json(file_path)
 dictdata=list_to_dict(file_path)
 show_setosa(file_path)
+min_max_area(file_path)
     
