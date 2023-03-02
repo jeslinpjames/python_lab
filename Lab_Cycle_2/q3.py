965297680ea2ae2c08a7d90b3366ed304f972f30
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-02 14:15:15 +0530

    read json

--- Lab_Cycle_2/q3.py
+++ Lab_Cycle_2/q3.py
@@ -1,1 +1,7 @@
 import json
+file_path='D:/git/python_lab/Lab_Cycle_2/iris.json'
+def read_json(file_path):    
+    with open(file_path,'r') as f:
+        data=json.load(f)
+        # print (data)
+    return data
