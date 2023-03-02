a882a87f1d591a5b1bfe8c81540f88c78e1a6647
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-02 17:06:33 +0530

    changed variable names

--- Lab_Cycle_2/q3.py
+++ Lab_Cycle_2/q3.py
@@ -2,17 +2,18 @@ import json
 file_path='D:/git/python_lab/Lab_Cycle_2/iris.json'
 def read_json(file_path):    
     with open(file_path,'r') as f:
-        data=json.load(f)
+        listdata=json.load(f)
     print("The list with each line of the file as a element is : \n")
-    for x in data:
+    for x in listdata:
         print(x,'\n')
-    return data
+    return listdata
 def list_to_dict(filepath):
     with open(filepath,'r') as f:
-        data=json.load(f)
+        dictdata=json.load(f)
     print("The file as a list of dictionary objects is : \n")
-    print (data)
-    return data
-data=read_json(file_path)
-data=list_to_dict(file_path)
+    print (dictdata)
+    return dictdata
+
+listdata=read_json(file_path)
+dictdata=list_to_dict(file_path)
     
