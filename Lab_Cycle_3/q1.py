1b618249763cef0c3f535e05f58b639dd0ab4021
jeslinpjames <jeslinpjames007@gmail.com>
2023-05-11 23:29:43 +0530

    game Window added

--- Lab_Cycle_3/q1.py
+++ Lab_Cycle_3/q1.py
@@ -1,10 +1,14 @@
 import pygame
 from sys import exit
+
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((600,700))
+pygame.display.set_caption('Tik Tac Toe')
+clock = pygame.time.Clock()
+
+
 while True:
-    for event in pygame.event.get(): 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit() 
-    pygame.display.update()
+            exit()
