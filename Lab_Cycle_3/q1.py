60554c2ffadcdc99cf79bc9184e6362e8ae96521
jeslinpjames <jeslinpjames007@gmail.com>
2023-05-12 11:18:29 +0530

    colour added

--- Lab_Cycle_3/q1.py
+++ Lab_Cycle_3/q1.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((600,700))
 pygame.display.set_caption('Tik Tac Toe')
 clock = pygame.time.Clock()
+game_active = True
 
 
 while True:
@@ -12,3 +13,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+    if game_active:
+        screen.fill((0,153,153))
+    pygame.display.update()
+    clock.tick(60)
